5a15f53d7e233b71d6db38c22d74db7976440cdb
SaraGarcesCespedes <sgarcesc@unal.edu.co>
2020-07-16 20:11:53 -0500

    Cosas útiles

--- R/otras cosas.R
+++ R/otras cosas.R
@@ -19,8 +19,18 @@ formulas_tmp <- as.list(formulas_corrector)
 fos_bind <- function(formula, response){
         paste(response, paste(formula, collapse = " "))
 }
+Y <- rnorm(100, 0, 1)
 fos_mat_char <- lapply(formulas_tmp, fos_bind, response = Y)
 fos_mat <- lapply(fos_mat_char, as.formula)
-list_mfs <- lapply(fos_mat, model.frame, data = data)
-data <- as.data.frame(matrix(1, nrow = 100, ncol = 2))
-colnames(data) <- c("y", "x")
+list_mfs <- lapply(fos_mat, model.frame, data = data1)
+#data <- as.data.frame(matrix(1, nrow = 100, ncol = 2))
+#colnames(data) <- c("y", "x")
+data1 <- NULL
+if ( is.null(data1) ){
+        data_reg <- as.data.frame(list_mfs)
+        var_names <- as.character(unlist(sapply(list_mfs, names)))
+        names(data_reg) <- var_names
+        data_reg <- as.data.frame(data_reg[,unique(var_names)])
+        names(data_reg) <- unique(var_names)
+        data <- data_reg
+}
