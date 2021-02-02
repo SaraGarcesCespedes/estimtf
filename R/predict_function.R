d10c8f995f426db84d58819a6115c7460e6f9a97
SaraGarcesCespedes <sgarcesc@unal.edu.co>
2021-02-02 07:13:00 -0500

    crear función predict para módulo glm

---
+++ R/predict_function.R
@@ -0,0 +1,74 @@
+#' @title predict.MLEtf function
+#'
+#' @description Function to produce result summaries of the estimates of parameters from statistical
+#' distributions using \code{\link{dist_estimtf}} or parameters from regression models using
+#' \code{\link{reg_estimtf}}.
+#'
+#' @author Sara Garces Cespedes
+#'
+#' @param object an object of class \code{MLEtf} for which a summary is desired.
+#' @param newdata an optional data frame in which to look variables with which to predict. If \code{newdata} is
+#' missing or \code{NULL}, the fitted values are used.
+#' @param ... additional arguments affecting the summary produced.
+#'
+#' @return The output from
+#'
+#' @details \code{predict.MLEtf} COMPLETAR
+#'
+#' @importFrom stats delete.response
+#'
+#' @examples
+#' #-------------------------------------------------------------
+#' # Estimation of both normal distrubution parameters
+#'
+#' #CAMBIAR EJEMPLO
+#' x <- rnorm(n = 1000, mean = 10, sd = 3)
+#'
+#' summary(dist_estimtf(x, xdist = "Normal", optimizer = "AdamOptimizer",
+#'                            hyperparameters = list(learning_rate = 0.1)))
+#'
+#' @rdname predict.MLEtf
+#' @export
+#------------------------------------------------------------------------
+# Predict function ------------------------------------------------------
+#------------------------------------------------------------------------
+predict.MLEtf <- function(object, newdata, ...) {
+
+        if (!inherits(object, "MLEtf")) {
+                warning("Object is not class MLEtf")
+        }
+
+        # error con newdata
+        if (missing(newdata) | is.null(newdata)) {
+                Y <- object$linear_response
+        } else {
+                if (!is.data.frame(newdata)) {
+                        stop("newdata must be a data frame")
+                } else {
+                        data <- newdata
+
+                        formula <- object$outputs$formula
+
+                        tt <- object$outputs$tt
+
+                        Terms <- delete.response(tt)
+
+                        xlevels <- object$outputs$xlevels
+
+                        data_reg_predict <- model.frame(Terms, data = data, xlev = xlevels)
+
+                        dsg_matrix_predict <- model.matrix(object = Terms, data = data_reg_predict)
+
+                        coeff_values <- object$coeff_estim
+
+
+                        # estimate Y
+                        Y <- dsg_matrix_predict %*% coeff_values
+                        Y <- as.numeric(Y)
+                }
+        }
+
+
+
+        return(Y)
+}
