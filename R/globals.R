0427c961040458f0df875e054e4aa69423b13c17
SaraGarcesCespedes <sgarcesc@unal.edu.co>
2021-04-18 11:32:07 -0500

    agregar plot

--- R/globals.R
+++ R/globals.R
@@ -1,2 +1,2 @@
 utils::globalVariables(c("tf", "dict", "tail", "ydist", "name", "use_locking", "n", "npar",
-                         "as.formula", "model.frame", "loss", "Iteration"))
+                         "as.formula", "model.frame", "loss", "Iteration", "plot"))
